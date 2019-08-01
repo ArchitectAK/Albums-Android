a4ede851c5231ebf9b9552c9bbdac24627793104
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-08-01 14:06:57 +0800

    refactored code

--- app/src/main/java/com/ankkumar/albums/utils/Utils.kt
+++ app/src/main/java/com/ankkumar/albums/utils/Utils.kt
@@ -3,7 +3,6 @@ package com.ankkumar.albums.utils
 object Utils {
 
     const val BASE_URL = "https://jsonplaceholder.typicode.com"
-    "
     const val ENDPOINT_ALBUM = "/albums"
 
     const val CONNECTION_TIMEOUT = 5000
